9db19f37767ad02ccff6717306ec555f0d99e0ed
frank <frank.huagen@gmail.com>
2018-06-27 17:53:08 +0200

    Created Eclipse Java project, (import)

---
+++ src/main/java/main.java
@@ -0,0 +1,15 @@
+import org.junit.Before;
+import net.sf.json.JSONObject;
+
+
+@SuppressWarnings("unused")
+public class main {
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.print("Hello World");
+	}
+
+}
