0ee544176857a40b4ff46960974c35a0ce34bcd7
frank <frank.haugen@gmail.com>
2018-07-03 23:06:53 +0200

    Small changes to Parser class

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -8,8 +8,15 @@ public class Parser {
 
     public JSONObject parse(JSONObject before, JSONObject after) {
         // TODO Implement this
+	
+	JSONObject var1 = new JSONObject();
+        
+	
+	
         JSONObject output = new JSONObject();
         
+	
+	
         output.accumulate("first", before);
         output.accumulate("second", after);
         
