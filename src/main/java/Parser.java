2d4840e10ebbd19266810e4bcab55ba750eb0810
frank <frank.haugen@gmail.com>
2018-07-04 13:56:36 +0200

    Worked on flattening json in parser

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,25 +1,37 @@
 //import net.sf.json.JSONObject;
+import java.util.List;
 import org.json.*;
 
 /**
  *
  */
 public class Parser {
-
+     
+     public List<Candidate> candidates;
+     
     public JSONObject parse(JSONObject before, JSONObject after) {
-        // TODO Implement this
+	  // TODO Implement this
+
+	  JSONObject output = new JSONObject();
+
+	  // Before
+
+	  JSONArray jsonArrayBefore = before.getJSONArray("candidates");
+
+
+	  for (int i = 0; i < jsonArrayBefore.length(); i++)
+	  {
+	       System.out.println(jsonArrayBefore.getJSONObject(i).get("candidateName"));
+	       
+	       
+	  }
 	
-	JSONObject var1 = new JSONObject();
-        
 	
 	
-        JSONObject output = new JSONObject();
-        
 	
 	
         output.accumulate("first", before);
-        output.accumulate("second", after);
-        
+	// Candidate cand = new Candidate(123, 0, Title, Name, 0, StartTime, EndTime);
 	
 	
 	
