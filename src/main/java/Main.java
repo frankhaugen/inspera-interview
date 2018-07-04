190f6723a1d6ff73ef4649c264c8234ede20d240
frank <frank.haugen@gmail.com>
2018-07-04 22:06:41 +0200

    Code cleaning

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,25 +2,10 @@ import org.json.*;
 
 public class Main {
      
-     // Testing variables
-     public static String testDT = "2016-01-20T10:00:00Z";
-     
-    // Instantiate a new Parser
-    public static Parser parser = new Parser();
-    
-    // Instantiate class time zone editor
-    public static TimeZoneConverter tzConverter = new TimeZoneConverter();
-    
     // Declare variables containing URIs to source data
     public static String jsonFileBeforeURI = "src\\main\\resources\\before.json";
     public static String jsonFileAfterURI = "src\\main\\resources\\after.json";
-    
-    // Declearing a variable containing the timezone-ID we want to convert date to
-    public static String timeZone = "Europe/Oslo";
-    
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
         
         // Decleared new JSONObjects
@@ -30,17 +15,11 @@ public class Main {
 	 try
 	 {
 	       // Display the parsed data in the console output
-	       System.out.println(parser.parse(jsonBefore, jsonAfter).toString(3));
+	       System.out.println(Parser.parse(jsonBefore, jsonAfter).toString(1));
 	 }
 	 catch (Exception e)
 	 {
 	      System.out.println("ERROR: " + e);
 	 }
-	
-	// Beautification
-	System.out.println("");
-	
-	// Display and convert the time based on timezone ID
-	System.out.println(tzConverter.Convert(testDT, timeZone));
     }
 }
--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -12,9 +12,8 @@ public class Parser {
      /**
       *
       */
-    //public TimeZoneConverter tzConverter = new TimeZoneConverter();
      
-    public JSONObject parse(JSONObject before, JSONObject after) {
+    public static JSONObject parse(JSONObject before, JSONObject after) {
 	 
 	  JSONObject inputMetaBefore = before.getJSONObject("meta");
 	  JSONObject inputMetaAfter = after.getJSONObject("meta");
@@ -44,149 +43,24 @@ public class Parser {
 	  JSONArray outputAdded = new JSONArray();
 	  JSONArray outputRemoved = new JSONArray();
 	  
-	  
-	  
 	  outputCandidates.put("edited", outputEdited);
 	  outputCandidates.put("added", outputAdded);
 	  outputCandidates.put("removed", outputRemoved);
 	  
+	  // TODO chack for changes
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
 	  output.put("meta", outputMeta);
 	  output.put("candidatates", outputCandidates);
 	  
-	  //System.out.println("Test output: " + before.getJSONArray("candidates"));
-	  //System.out.println("Test output: " + before.getJSONObject("meta"));
-	  //System.out.println("Test output: " + before.get("id"));
-	  
-	  JSONObject jsObj = new JSONObject();
-	  jsObj.append("candidates", before.getJSONArray("candidates"));
-	  //jsObj.append("candidates", after.getJSONArray("candidates"));
-	  
-	  for (int i = 0; i < after.getJSONArray("candidates").length(); i++)
-	 {
-	      if (jsObj.similar(after.getJSONArray("candidates").get(i)))
-	      {
-		   System.out.println("TEST!");
-	      }
-	 }
-	  
-	  
-	  
-	  //System.out.println(jsObj);
-	  
-	  for (Object candJSONObject : before.getJSONArray("candidates"))
-	  {
-	       //System.out.println(candJSONObject);
-	  }
-	  
-	  for (Object candJSONObject : after.getJSONArray("candidates"))
-	  {
-	       
-	  }
-	  
-	  /*
-	  Meta
-	       ()
-	       ()
-	  Candidates
-	       ()
-	       ()
-	       ()
-	  
-	  /*
-	  
-	  {
-	       "meta": [
-		 {
-		   "field": "title",
-		   "before": "Title",
-		   "after": "New Title"
-		 },
-		 {
-		   "field": "endTime",
-		   "before": "2016-01-20T18:00:00+02",
-		   "after": "2016-01-20T20:00:00+02"
-		 }
-	       ],
-	       "candidates": {
-		 "edited": [
-		   {
-		     "id": 10
-		   },
-		   {
-		     "id": 11
-		   }
-		 ],
-		 "added": [
-		   {
-		     "id": 13
-		   }
-		 ],
-		 "removed": [
-		   {
-		     "id": 12
-		   }
-		 ]
-	       }
-	     }
-	  
-	  */
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  List<Candidate> beforeList = CreateCandidateList(before);
-	  List<Candidate> afterList = CreateCandidateList(after);
-	  List<Candidate> removedList = new ArrayList<Candidate>();
-	  
-//	  System.out.println(beforeList.toString());
-//	  for (Candidate candidate : beforeList)
-//	 {
-//	      System.out.println(candidate.ID);
-//	 }
-//	  System.out.println(afterList.toString());
-//	  for (Candidate candidate : afterList)
-//	 {
-//	      System.out.println(candidate.ID);
-//	 }
-//	  
-//	  
-//	  for (Candidate cand : afterList)
-//	 {
-//	      if (beforeList.contains(cand.ID))
-//	      {
-//		   System.out.println(cand.ID + " Is still here");
-//	      }
-//	 }
-	  
-	  
-	  
-	  for (int i = 0; i < beforeList.size(); i++)
-	  {
-	       //System.out.println("Checking: " + beforeList.get(i).ID);
-	       for (int j = 0; j < afterList.size(); j++)
-	       {
-		    //System.out.println("With: " + afterList.get(j).ID);
-		    if (beforeList.get(i).ID == afterList.get(j).ID)
-		    {
-			 //System.out.println(beforeList.get(i).ID + " is the same as: " + afterList.get(j).ID);
-		    }
-	       }
-	  }
-	  
-	  
-	  
-	  
-	  
-	  
-	  //output.accumulate("first", before);
-	  
-	  // Before
-	  
-	  
-	  //System.out.println(candidates.toString());
+
         return output;
     }
     
