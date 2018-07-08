4acb428f9b2b094164d03774513394c7f1885b71
frank <frank.haugen@gmail.com>
2018-07-08 21:58:55 +0200

    Configured mores tests

--- src/test/java/ParserTest.java
+++ src/test/java/ParserTest.java
@@ -1,12 +1,7 @@
-//import jdk.nashorn.internal.runtime.RewriteException;
 import org.json.JSONObject;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
-import org.junit.gen5.api.Disabled;
-import org.junit.gen5.commons.*;
-//import org.junit.Assert.*;
 import org.skyscreamer.jsonassert.*;
 
 /**
@@ -36,20 +31,40 @@ public class ParserTest {
 	
 	// Get the result from the parser
 	jsonResult = Parser.parse(jsonBefore,jsonAfter);
-
-	  
     }
+    
+    // Test to see if the collection of text from file is working properly
+    @Test
+    public void testFiletextCollector()
+    {
+	 assertEquals("foo!bar", FiletextCollector.Collect("src\\test\\resources\\testFile.txt"));
+    }
+    
+    // Test to see if edited time based on timezone-id returns correct values
+    @Test
+    public void testTimezoneConversion()
+    {
+	 assertEquals("2016-01-20T17:00:00+01" , TimeZoneConverter.Convert(jsonBefore.getJSONObject("meta").getString("endTime"), "Europe/Oslo"));
+    }
+    
+    // Test to see if the ca is collected correctly
+    @Test
+    public void testJsonExtractEditedCandidates()
+    {
+	 JSONAssert.assertEquals(jsonResult.get("candidates").toString(), jsonDiff.get("candidates").toString(), JSONCompareMode.LENIENT);
+    }
+    
     
     // Test to see if the metadata is collected correctly
     @Test
-    public void testIfJsonMetaData()
+    public void testJsonMetaDataMelding()
     {
 	 JSONAssert.assertEquals(jsonResult.get("meta").toString(), jsonDiff.get("meta").toString(), JSONCompareMode.LENIENT);
     }
     
     // Test to see if the whole parser works as planned
     @Test
-    public void testIfJsonParser()
+    public void testJsonParser()
     {
 	 JSONAssert.assertEquals(jsonResult, jsonDiff, JSONCompareMode.LENIENT);
     }
