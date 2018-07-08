e40327db08bb253fa90cbf3d0e8fd66fe96f7aac
frank <frank.haugen@gmail.com>
2018-07-09 01:44:46 +0200

    Commenting and beatification

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -28,6 +28,7 @@ public class Parser {
 	  JSONObject output = new JSONObject();
 	  
 	  // Create JSONObject that contains merged metadata
+	  // Calls on the method GetMetadata
 	  JSONArray outputMeta = GetMetadata(jsonBefore.getJSONObject("meta"), jsonAfter.getJSONObject("meta"), new String[] {"title","endTime"});
 	  
 	  // Collect data about changes to candidates list
@@ -43,6 +44,7 @@ public class Parser {
      
      /**
       * Extracts metadata fields from two jsonobject containing the keys given
+      * Flexible, if the data should change length
       * 
       * @param inputBefore First jsonObject
       * @param inputAfter Second jsonObject
@@ -53,20 +55,21 @@ public class Parser {
      {
 	  JSONArray output = new JSONArray();
 	  
-	  //String[] metadataFields = {"title","endTime"};
-	  
 	  // Use metadate keys to extract values specified above
 	  for (String metadataField : metadataFields)
 	  {
 	       JSONObject metaData = new JSONObject();
 	       metaData.put("field", metadataField);
 	       
+	       // Ineligant yet functional
+	       // Attempt to convert the given string to a datetime string, on failure, input the value found
 	       try{ metaData.put("before", TimeZoneConverter.Convert(inputBefore.get(metadataField).toString(), "Europe/Oslo")); } 
 	       catch (Exception e) { metaData.put("before", inputBefore.get(metadataField)); }
 	       
 	       try{ metaData.put("after", TimeZoneConverter.Convert(inputAfter.get(metadataField).toString(), "Europe/Oslo")); } 
 	       catch (Exception e) { metaData.put("after", inputAfter.get(metadataField)); }
 	       
+	       // Atttatch metadata to output
 	       output.put(metaData);
 	  }
 	  
@@ -83,42 +86,42 @@ public class Parser {
       */
      public static JSONObject GetCandidates(JSONObject inputBefore, JSONObject inputAfter)
      {
+	  // Create the JSONObject to be be outputted
 	  JSONObject output = new JSONObject();
+	  
 	  
 	  JSONArray CandidatesBefore = inputBefore.getJSONArray("candidates");
 	  JSONArray CandidatesAfter = inputAfter.getJSONArray("candidates");
 	  
-	  // Add edited candidates to output JsonObject
+	  // Add edited candidates to output JsonObject directly
 	  output.put("edited", GetEditedCadidates(inputBefore.getJSONArray("candidates"), inputAfter.getJSONArray("candidates")));
 	  
-	  // Add removed to JSONArray
+	  // Add removed to JSONArray, (only removed candidates are left in the before json data
 	  JSONArray outputRemoved = new JSONArray();
 	  CandidatesBefore.forEach(a ->
 	  {
-	       JSONObject candBefore = (JSONObject)a;
-	       outputRemoved.put(new JSONObject().put("id", candBefore.getInt("id")));
+	       JSONObject candidateBefore = (JSONObject)a;
+	       outputRemoved.put(new JSONObject().put("id", candidateBefore.getInt("id")));
 	  });
+	  output.put("removed", outputRemoved); // Add to output
 	  
-	  // Add added to JSONArray
+	  // Add edited to JSONArray, (only added candidates are left in the after json data
 	  JSONArray outputAdded = new JSONArray();
 	  CandidatesAfter.forEach(a ->
 	  {
-	       JSONObject candAfter = (JSONObject)a;
-	       outputAdded.put(new JSONObject().put("id", candAfter.getInt("id")));
+	       JSONObject candidateAfter = (JSONObject)a;
+	       outputAdded.put(new JSONObject().put("id", candidateAfter.getInt("id")));
 	  });
+	  output.put("added", outputAdded); // Add to output
 	  
-	  
-	  // Collect the different types for 
-	  output.put("added", outputAdded);
-	  output.put("removed", outputRemoved);
-	  
+	  // Return JsonObject with the trhee types of candidates
 	  return output;
      }
      
      /**
       * Gets the candidates that are in both arrays, and deletes them,
-      * (Deteltion functionality was accidentaly brought when separration out to a method,
-      * It is unclear why the deletions work)
+      * (The Deletion functionality was accidentally brought when separation out to a method,
+      * It is unclear why the deletions work, but my theory is that the inputted values are references and not new objects)
       * 
       * @param inputBefore JsonArray containing a list of candidates
       * @param inputAfter JsonArray containing a list of candidates
@@ -126,44 +129,46 @@ public class Parser {
       */
      public static JSONArray GetEditedCadidates(JSONArray inputBefore, JSONArray inputAfter)
      {
+	  // Create the JSONObject to be retured
 	  JSONArray output = new JSONArray();
 	  
 	  // Check for candidates existing i both arrays
+	  // Use a foreach loop to iterate through to delete the candidates present in both Before and After .json files
 	  inputBefore.forEach(a ->
 	  {
-	       JSONObject candBefore = (JSONObject)a;
+	       JSONObject candidateBefore = (JSONObject)a; // must create a new JSONObject, as "a" is not recognized as such
 	       inputAfter.forEach(b ->
 	       {
-		    JSONObject candAfter = (JSONObject)b;
-		    if (candBefore.get("id") == candAfter.get("id"))
+		    JSONObject candidateAfter = (JSONObject)b;
+		    if (candidateBefore.get("id") == candidateAfter.get("id"))
 		    {
-			 output.put(new JSONObject().put("id", candBefore.getInt("id")));
+			 output.put(new JSONObject().put("id", candidateBefore.getInt("id"))); // Add the candidate locadet in both arrays to the output array
 		    }
 	       });
 	  });
 	  
-	  // Delete edited entries to simplify the process of sort added and deleted
-	  // Unclear how this part sgould work on a variable set in a different method
+	  // Function but not pretty
+	  // Delete edited entries to simplify the process of seperating added and deleted
+	  // Unclear how this part should work on a variable set in a different method
 	  output.forEach(c ->
 	  {
 	       for (int i = 0; i < inputBefore.length(); i++)
 	       {
-		    JSONObject candTest = (JSONObject)c;
-		    if (inputBefore.getJSONObject(i).get("id") == candTest.get("id") )
+		    JSONObject candidate = (JSONObject)c;
+		    if (inputBefore.getJSONObject(i).get("id") == candidate.get("id") )
 		    {
 			 inputBefore.remove(i); // Why does this work?
 		    }
 	       }
 	       for (int i = 0; i < inputAfter.length(); i++)
 	       {
-		    JSONObject candTest = (JSONObject)c;
-		    if (inputAfter.getJSONObject(i).get("id") == candTest.get("id") )
+		    JSONObject candidate = (JSONObject)c;
+		    if (inputAfter.getJSONObject(i).get("id") == candidate.get("id") )
 		    {
 			 inputAfter.remove(i);  // Why does this work?
 		    }
 	       }
 	  });
-	  
 	  
 	  // returns a JsonArray with the candidates that are in both JsonArrays
 	  return output;
