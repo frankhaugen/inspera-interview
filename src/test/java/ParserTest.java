a706962a26f035ce1d39594fb4c7b5c65416bb3d
frank <frank.haugen@gmail.com>
2018-07-08 19:58:59 +0200

    Experimented

--- src/test/java/ParserTest.java
+++ src/test/java/ParserTest.java
@@ -27,20 +27,33 @@ public class ParserTest {
       */
      @Before
     public void setup() {
-        // Load in test data from before.json and after.json
+        
+	// Load in test data from before.json and after.json
 	jsonBefore = new JSONObject(FiletextCollector.Collect("src\\main\\resources\\before.json"));
 	jsonAfter = new JSONObject(FiletextCollector.Collect("src\\main\\resources\\after.json"));
 	
 	// Loed in test-data for validation from diff.json
 	jsonDiff = new JSONObject(FiletextCollector.Collect("src\\main\\resources\\diff.json"));
 	
-	
-	jsonResult = Parser.parse(jsonBefore,jsonAfter) ;
+	jsonResult = Parser.parse(jsonBefore,jsonAfter);
+
+	  
     }
     
     // Test to see if 
     @Test
-    public void testIfJsonIsParsedCorrectly()
+    public void testIfJsonIsParsedCorrectly2()
+    {
+	 System.out.println("Diff + Result:");
+	 System.out.println(jsonDiff.toString(0));
+	 System.out.println(jsonResult.toString(0));
+	 
+	 JSONAssert.assertEquals(jsonResult, jsonDiff, JSONCompareMode.LENIENT);
+    }
+    
+    // Test to see if 
+    @Test
+    public void testIfJsonMetaDataIsCorrect()
     {
 	 System.out.println("Diff + Result:");
 	 System.out.println(jsonDiff.toString(0));
