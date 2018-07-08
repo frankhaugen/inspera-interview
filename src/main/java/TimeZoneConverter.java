6bf602b6b8f23eaaf0ed1c49e096f2cce312ab18
frank <frank.haugen@gmail.com>
2018-07-09 01:52:15 +0200

    Commenting and beatification

--- src/main/java/TimeZoneConverter.java
+++ src/main/java/TimeZoneConverter.java
@@ -7,18 +7,26 @@ import java.time.format.DateTimeFormatter;
  */
 public class TimeZoneConverter
 {
+
+     /**Convert a datetime string to another timezone
+      *
+      * @param inputTime The datetime string to be changed. format: "2016-01-20T18:00:00Z"
+      * @param zoneId The string value for the timezone to convert to. e.g. "Europe/Oslo"
+      * @return A DateTime string with which has been offset to the time zone specified
+      */
      public static String Convert(String inputTime, String zoneId)
      {
 	  // Formatter to specify the returned datetime strings's format
-	  DateTimeFormatter dtFormatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
+	  DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
 	  
-	  // Convert inputtedt datetime-string to a recognizable format before manipulation. Instant is the most logical format
+	  // Convert inputted datetime-string to a recognizable format before manipulation. Instant is the most logical format
 	  Instant instant = Instant.parse(inputTime);
 	  
 	  // Convert Instant to another DateTime format and changing the time to
 	  ZonedDateTime zonedDateTime = instant.atZone(ZoneId.of(zoneId));
 	  
-	  String output = zonedDateTime.format(dtFormatter);
+	  // create a string for the output using a formatter to format the datetime-string
+	  String output = zonedDateTime.format(dateTimeFormatter);
 	  return output.substring(0, output.length() - 3); // Cheat to get the wanted format for date, (+01 in stead of +01:00 which is proper)
      }
 }
