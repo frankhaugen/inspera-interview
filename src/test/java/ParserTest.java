0749e940d65aa6ef6238938accb86826d8e2adec
frank <frank.haugen@gmail.com>
2018-07-08 13:37:40 +0200

    Preparing to implement junit

--- src/test/java/ParserTest.java
+++ src/test/java/ParserTest.java
@@ -1,28 +1,47 @@
-import jdk.nashorn.internal.runtime.RewriteException;
+//import jdk.nashorn.internal.runtime.RewriteException;
+import org.json.JSONObject;
 import org.junit.Before;
+import org.junit.Test;
+import org.junit.gen5.commons.*;
+//import org.junit.Assert.*;
+import org.skyscreamer.jsonassert.*;
 
 /**
  *
+ * @author frank
  */
 public class ParserTest {
 
-    Parser parser = new Parser();
+    static Parser parser = new Parser();
+    
+    static JSONObject jsonBefore;
+    static JSONObject jsonAfter;
+    static JSONObject jsonDiff;
+    
+    static JSONObject jsonResult;
 
-    @Before
-    public void setup() {
-        // TODO Load in test data from before.json and after.json
+     /**
+      *
+      */
+     @Before
+    public static void setup() {
+        // Load in test data from before.json and after.json
+	jsonBefore = new JSONObject(FiletextCollector.Collect("src\\main\\resources\\before.json"));
+	jsonAfter = new JSONObject(FiletextCollector.Collect("src\\main\\resources\\after.json"));
+	
+	// Loed in test-data for validation from diff.json
+	jsonDiff = new JSONObject(FiletextCollector.Collect("src\\main\\resources\\diff.json"));
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	jsonResult = parser.parse(jsonBefore,jsonAfter) ;
+    }
+    
+    @Test
+    public void testIfJsonIsParsedCorrectly()
+    {
+	 JSONAssert.assertEquals(jsonResult, jsonDiff, JSONCompareMode.LENIENT);
+	 
+		 
     }
 
     // TODO Define tests here
